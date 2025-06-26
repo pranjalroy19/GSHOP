6eed43d71328660abd3e98e649be1e891740532d
pranjalroy19 <161058404+pranjalroy19@users.noreply.github.com>
2025-06-26 18:00:13 +0530

    Update Login.js

--- e_commerce/src/components/Auth/Login.js
+++ e_commerce/src/components/Auth/Login.js
@@ -15,7 +15,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("app/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
